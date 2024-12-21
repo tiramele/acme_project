ab65f239aa0de54ad04d6c4642ea7b3e52eabf29
Tiramele <tiramele@gmail.com~>
2024-12-21 16:57:15 +0300

    Форма

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class BirthdayForm(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(required=False)
-    birthday = forms.DateField() 
+    birthday = forms.DateField()
