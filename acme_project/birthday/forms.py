8c55bb1d29e6546a3f85521cc7849d528e5ecef5
Tiramele <tiramele@gmail.com~>
2024-12-30 17:38:03 +0300

    Итог

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -3,7 +3,12 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(required=False)
-    birthday = forms.DateField()
-    
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        # Указываем, что виджет для ввода даты должен быть с типом date.
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
