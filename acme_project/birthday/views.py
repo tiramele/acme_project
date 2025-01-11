220e4226c627447876cdf5793ef10b2ff33bc406
Tiramele <tiramele@gmail.com~>
2025-01-11 18:00:07 +0300

    включение удаления

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,12 +1,30 @@
 # birthday/views.py
 # Импортируем шорткат для получения объекта или вызова 404 ошибки.
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 # Импортируем модель дней рождения.
 from .models import Birthday
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
+
 
 # Добавим опциональный параметр pk.
 def birthday(request, pk=None):
@@ -27,14 +45,11 @@ def birthday(request, pk=None):
     # Сохраняем данные, полученные из формы, и отправляем ответ:
     if form.is_valid():
         form.save()
-        # ...вызовем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+    return render(request, 'birthday/birthday.html', context) 
 
 
 def birthday_list(request):
